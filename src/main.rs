a43dcb6023d33fbdf299ba133adb19ac5951b897
GimmeDataNow <alan.omazic@gmail.com>
2023-09-03 21:53:14 +0300

    fullscreen and fancy painting

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,8 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
+use bresenham;
+
 fn main() -> Result<(), Error> {
     let mut cam_pos = (0, 0);
     let mut fps_tracker = fps_counter::FPSCounter::new();
@@ -42,10 +44,12 @@ fn main() -> Result<(), Error> {
         WindowBuilder::new()
             .with_title("Re-Noita")
             .with_inner_size(size)
-            .with_min_inner_size(size)
+            //.with_min_inner_size(size)
             .build(&event_loop)
             .unwrap()
     };
+    let mut monitor_index = 0;
+    let mut monitor = event_loop.available_monitors().next().expect("well damn");
 
     window.set_cursor_icon(winit::window::CursorIcon::Crosshair);
 
@@ -61,7 +65,6 @@ fn main() -> Result<(), Error> {
             scale_factor,
             &pixels,
         );
-
         (pixels, framework)
     };
 
@@ -89,40 +92,81 @@ fn main() -> Result<(), Error> {
         if input.update(&event) {
 
             // Exit events
-            if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-            if let Some(scale_factor) = input.scale_factor() {
-                framework.scale_factor(scale_factor);
+            if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() { *control_flow = ControlFlow::Exit; return; }
+
+            // window scaling
+            if let Some(scale_factor) = input.scale_factor() { framework.scale_factor(scale_factor) }
+
+            // key binds || fulscreen may not work on mac_os, but fuck mac_os
+            {
+                map_key!(VirtualKeyCode::W, VirtualKeyCode::Up,     cam_pos= (cam_pos.0, cam_pos.1 + 1));
+                map_key!(VirtualKeyCode::A, VirtualKeyCode::Left,   cam_pos= (cam_pos.0 - 1, cam_pos.1));
+                map_key!(VirtualKeyCode::S, VirtualKeyCode::Down,   cam_pos= (cam_pos.0, cam_pos.1 - 1));
+                map_key!(VirtualKeyCode::D, VirtualKeyCode::Right,  cam_pos= (cam_pos.0 + 1, cam_pos.1));
+                map_key!(VirtualKeyCode::F11, window.set_fullscreen(Some(winit::window::Fullscreen::Borderless(Some(monitor.clone())))));
+                map_key!(VirtualKeyCode::F12, window.set_fullscreen(None));
+                map_key!(VirtualKeyCode::Return, {simulation_space.set_cell_at_global_coords(cam_pos, Cell::build_cell(chunk_manager::chunks::cells::CellType::Acid));});
             }
 
-            //key binds
-            map_key!(VirtualKeyCode::W, VirtualKeyCode::Up,     cam_pos= (cam_pos.0, cam_pos.1 + 1));
-            map_key!(VirtualKeyCode::A, VirtualKeyCode::Left,   cam_pos= (cam_pos.0 - 1, cam_pos.1));
-            map_key!(VirtualKeyCode::S, VirtualKeyCode::Down,   cam_pos= (cam_pos.0, cam_pos.1 - 1));
-            map_key!(VirtualKeyCode::D, VirtualKeyCode::Right,  cam_pos= (cam_pos.0 + 1, cam_pos.1));
-            map_key!(VirtualKeyCode::Return, {simulation_space.set_cell_at_global_coords(cam_pos, Cell::build_cell(chunk_manager::chunks::cells::CellType::Acid));});
-            map_key!(VirtualKeyCode::F12, simulation_space.simple_save(&(0, 0)));
+            // mouse inputs
+            {
+                let (mouse_cell, mouse_prev_cell) = input
+                .mouse()
+                .map(|(mx, my)| {
+                    let (dx, dy) = input.mouse_diff();
+                    let prev_x = mx - dx;
+                    let prev_y = my - dy;
+
+                    let (mx_i, my_i) = pixels
+                        .window_pos_to_pixel((mx, my))
+                        .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
+
+                    let (px_i, py_i) = pixels
+                        .window_pos_to_pixel((prev_x, prev_y))
+                        .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
+
+                    (
+                        (mx_i as isize, my_i as isize),
+                        (px_i as isize, py_i as isize),
+                    )
+                })
+                .unwrap_or_default();
+
+                // bad painting mode
+                if input.mouse_held(0) {
+                    simulation_space.set_cell_at_global_coords((mouse_cell.0 as i32 + cam_pos.0 - config::SCREEN_WIDTH/2, -mouse_cell.1 as i32 + cam_pos.1 + config::SCREEN_HEIGHT/2), Cell::build_cell(chunk_manager::chunks::cells::CellType::Acid));
+                }
+
+                // fancy painting mode
+                if input.mouse_held(1) || input.mouse_released(1) {
+                    for (x, y) in bresenham::Bresenham::new(mouse_prev_cell, mouse_cell) {
+                        simulation_space.set_cell_at_global_coords((x as i32 + cam_pos.0 - config::SCREEN_WIDTH/2, -y as i32 + cam_pos.1 + config::SCREEN_HEIGHT/2), Cell::build_cell(chunk_manager::chunks::cells::CellType::Acid));
+                    }
+                }
+            }
             
-            // Resize the window
+
+            // resize the window | errors are dbg only
             if let Some(size) = input.window_resized() {
                 if let Err(err) = pixels.resize_surface(size.width, size.height) {
-                    println!("pixels.resize_surface {}", err);
+                    dbg!("pixels.resize_surface {}", err);
                     *control_flow = ControlFlow::Exit;
                     return;
                 }
                 framework.resize(size.width, size.height);
             }
             
-            pixels.frame_mut();
+            // step by step simulation
             if !step_by_frame {
                 //simulation_space.update_cell_behaviour();
                 //simulation_space.update_cell_alchemy();
             }
+
+            // update display
             window.request_redraw();
         }
 
+        // window events
         match event {
             Event::WindowEvent { event, .. } => {
                 // Update egui inputs
@@ -149,7 +193,7 @@ fn main() -> Result<(), Error> {
 
                 // Basic error handling
                 if let Err(err) = render_result {
-                    println!("pixels.render {}", err);
+                    dbg!("pixels.render {}", err);
                     *control_flow = ControlFlow::Exit;
                 }
             }
